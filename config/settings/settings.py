72d066612008c2e8e4cb221bc76b835654287ea0
baejvv <jwbae0@naver.com>
2025-02-09 23:40:49 +0900

    django 프로젝트 구조 잡기

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'llm_project.urls'
+ROOT_URLCONF = 'llm_project.config.urls'
 
 TEMPLATES = [
     {
